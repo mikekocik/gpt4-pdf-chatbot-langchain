37e94ae828a006323d20ac54bcffb43656d9ba40
Mike Kocik <mike@kocik.ru>
2023-06-14 17:23:00 +0700

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this document?',
+        message: 'Hi, what would you like to learn about crypto regulation?',
         type: 'apiMessage',
       },
     ],
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Docs
+            Crypto Regulation Bot
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -174,7 +174,7 @@ export default function Home() {
                           </ReactMarkdown>
                         </div>
                       </div>
-                      {message.sourceDocs && (
+                      {/*message.sourceDocs && (
                         <div
                           className="p-5"
                           key={`sourceDocsAccordion-${index}`}
@@ -203,7 +203,7 @@ export default function Home() {
                             ))}
                           </Accordion>
                         </div>
-                      )}
+                      )*/}
                     </>
                   );
                 })}
@@ -261,8 +261,8 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="https://dna.partners">
+            Powered by LangChainAI. &copy; 2023 D&A Partners.
           </a>
         </footer>
       </Layout>
